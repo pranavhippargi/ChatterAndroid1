ba2a542d370c96f218139a9495afb59d6e7860e9
Pranav <jepranav@gmail.com>
2017-06-12 21:05:01 -0700

    With Firebase added

--- app/src/main/java/pranav/hippargi/chatter/RegisterActivity.java
+++ app/src/main/java/pranav/hippargi/chatter/RegisterActivity.java
@@ -1,9 +1,20 @@
 package pranav.hippargi.chatter;
 //test
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -12,16 +23,62 @@ public class RegisterActivity extends AppCompatActivity {
     private TextInputLayout mPassword;
     private Button mCreateBtn;
 
+    //FIREBASE
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        // FIREBASE
+        mAuth = FirebaseAuth.getInstance();
+
+
         mDisplayName = (TextInputLayout)findViewById(R.id.reg_display_name);
         mEmail = (TextInputLayout)findViewById(R.id.reg_email);
         mPassword = (TextInputLayout)findViewById(R.id.reg_password);
+
+        //Button
         mCreateBtn = (Button)findViewById(R.id.reg_create_btn);
+
+        mCreateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String display_name = mDisplayName.getEditText().getText().toString();
+                String email = mEmail.getEditText().getText().toString();
+                String password = mPassword.getEditText().getText().toString();
+
+                registerUser(display_name, email, password);
+            }
+        });
+
+    }
+
+    private void registerUser(String display_name, String email, String password) {
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful()){
+
+                    Intent mainIntent = new Intent(RegisterActivity.this, MainActivity.class);
+                    startActivity(mainIntent);
+                    finish();
+
+                } else {
+
+                    Toast.makeText(RegisterActivity.this, "You got some error.", Toast.LENGTH_LONG).show();
+
+                }
+            }
+        });
 
     }
 }
