8867b81f3112d6377fa57e863bb1f1e23457f920
Pranav <jepranav@gmail.com>
2017-06-12 19:23:35 -0700

    COOL!

--- app/src/main/java/pranav/hippargi/chatter/RegisterActivity.java
+++ app/src/main/java/pranav/hippargi/chatter/RegisterActivity.java
@@ -1,13 +1,27 @@
 package pranav.hippargi.chatter;
 //test
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private TextInputLayout mDisplayName;
+    private TextInputLayout mEmail;
+    private TextInputLayout mPassword;
+    private Button mCreateBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mDisplayName = (TextInputLayout)findViewById(R.id.reg_display_name);
+        mEmail = (TextInputLayout)findViewById(R.id.reg_email);
+        mPassword = (TextInputLayout)findViewById(R.id.reg_password);
+        mCreateBtn = (Button)findViewById(R.id.reg_create_btn);
+
     }
 }
